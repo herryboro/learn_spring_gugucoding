b00693f6600dabb234db1db6ab53ec129f9e45e9
Herryboro <herryboro@naver.com>
2021-03-03 17:39:03 +0900

    - spring-mvc 관련 셋팅 - spring mvc 기본 흐름( DispatcherServlet -> Handler Mappin -> Controller -> ViewResolver 등등 ) - 관련 어노테이션 @ RequestMapping, @GetMapping, @PostMapping, model 등등

--- ex00/src/main/java/org/zerock/sample/Hotel.java
+++ ex00/src/main/java/org/zerock/sample/Hotel.java
@@ -2,18 +2,26 @@ package org.zerock.sample;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
 import lombok.ToString;
 
 @Component
 @ToString
+@AllArgsConstructor
 public class Hotel {
 	
 	private Chef chef;
+	private Restaurant restaurant;
 
-	public Hotel(Chef chef) {
-		super();
-		this.chef = chef;
-	}
+//	public Hotel(Chef chef) {
+//		super();
+//		this.chef = chef;
+//	}
+	
+//	public Hotel(Restaurant restaurant) {
+//		super();
+//		this.restaurant = restaurant;
+//	}
 	
 	
 }
