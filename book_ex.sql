ff2f8381b15457f5ef1f420b7c124a8b5f39ee78
Herryboro <herryboro@naver.com>
2021-03-05 19:57:05 +0900

    웹 mvc 설정  - mapper를 통한 mybatis select

--- book_ex.sql
+++ book_ex.sql
@@ -1,3 +1,14 @@
 create user book_ex identified by book_ex;
 grant connect, dba to book_ex;
 exec dbms_xdb.sethttpport(9090);
+
+create sequence seq_board;
+?
+create table tbl_board(
+    bno number(10,0),
+    title varchar2(200) not null,
+    content varchar2(2000) not null,
+    writer varchar2(50) not null,
+    regdate date default sysdate, 
+    updatedate date default sysdate
+);
