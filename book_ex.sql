a9a0ddd70c2ad17647beb7352eb9636ab4878424
Herryboro <herryboro@naver.com>
2021-03-08 20:18:44 +0900

    WebApplicationContext,  test에서  가상으로 url과 파라미터를 넘기기 위한 MockMvcRequest 학습

--- book_ex.sql
+++ book_ex.sql
@@ -3,7 +3,7 @@ grant connect, dba to book_ex;
 exec dbms_xdb.sethttpport(9090);
 
 create sequence seq_board;
-?
+
 create table tbl_board(
     bno number(10,0),
     title varchar2(200) not null,
@@ -20,9 +20,10 @@ values (seq_board.nextval, '테스트 제목','테스트 내용','user00');
 
 commit;
 
-select * from tbl_board;
+select * from tbl_board where bno > 0 order by bno asc;
+
 select sysdate from dual;
 select * from tbl_board where bno > 0;
-
+select seq_board.nextval from dual;
 
 
