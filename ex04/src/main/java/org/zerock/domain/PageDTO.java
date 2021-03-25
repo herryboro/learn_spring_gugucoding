959f8265f20d6a77c412e8d7cc0ee556b0a68e65
Herryboro <herryboro@naver.com>
2021-03-25 12:35:12 +0900

    AOP 학습   - proxy, target, joinpoint     ->  target: 순수한 비지니스 로직     -> proxy: 중간의 필요한 관심사들을 거쳐 target을 호출     -> joinpoint : target이 가진 여러 메서드   - advice, after, around 등의 어노테이션 기능 학습   - 스프링 트렌젝션

---
+++ ex04/src/main/java/org/zerock/domain/PageDTO.java
@@ -0,0 +1,41 @@
+package org.zerock.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class PageDTO {
+	private int startPage;
+	private int endPage;
+	private boolean prev, next;
+	private int total;
+	private Criteria cri;
+	
+	public PageDTO(int total, Criteria cri) {
+		System.out.println("--------------pageDto-------------");
+		System.out.println("total: " + total + ", cri.getPageNum(): " + cri.getPageNum() + ", cri.getAmount(): " + cri.getAmount());
+		this.total = total;
+		this.cri = cri;
+		this.endPage = (int)(Math.ceil(cri.getPageNum() / 10.0)) * 10;
+		System.out.println("endPage: " + endPage);
+		
+		this.startPage = this.endPage - 9;
+		System.out.println("startPage: " + startPage);
+		
+		int realEnd = (int)(Math.ceil(total * 1.0 / cri.getAmount()));
+		System.out.println("realEnd: " + realEnd);
+		
+		if(realEnd < this.endPage) {
+			this.endPage = realEnd;
+			System.out.println("realEnd가 endPage보다 작았을 경우의 새로운 endPage: " + endPage);
+		}	
+		
+		this.prev = this.startPage > 1;
+		System.out.println("prev: " + prev);
+		this.next = this.endPage < realEnd;
+		System.out.println("next: " + next);
+		System.out.println("--------------/pageDto-------------");
+	}
+	
+}
---
+++ ex04/src/main/java/org/zerock/service/BoardServiceimpl.java
@@ -0,0 +1,61 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+@AllArgsConstructor
+public class BoardServiceimpl implements BoardService {
+
+	// Spring 4.3 이상에서 자동처리
+	private BoardMapper mapper;
+	
+	@Override
+	public void register(BoardVO board) {
+		System.out.println("한글깨짐 확인: " + board.getContent());
+		System.out.println("한글깨짐 확인: " + board.getTitle());
+		log.info("register...." + board);
+		mapper.insertSelectKey(board);
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		log.info("get......" + bno);
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		log.info("modify......");
+		return mapper.update(board) == 1;
+	}
+	
+	@Override
+	public boolean remove(Long bno) {
+		log.info("remove.......");
+		return mapper.delete(bno) == 1;
+	}
+	
+	@Override
+	public List<BoardVO> getList(Criteria cri) {
+		log.info("get List with Criteria: " + cri);
+		for(String word : cri.getTypeArr()) {
+			System.out.println("cri.getTypeArr(): " + word);
+		}		
+		return mapper.getListWithPaging(cri);
+	}
+	
+	@Override
+	public int getTotal(Criteria cri) {
+		log.info("get total count");
+		return mapper.getTotalCount(cri);
+	}
+}
